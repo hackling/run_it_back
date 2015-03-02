b1ecd0f46909ba5aafbc2d40bc961b7c5c0fda79
Nicholas Chmielewski <hackling@gmail.com>
2015-03-02 10:54:06 +1000

    Add Rerun formatter

---
+++ lib/run_it_back.rb
@@ -0,0 +1,42 @@
+require 'rspec'
+require 'rspec/core/formatters/base_text_formatter'
+
+class RunItBack < RSpec::Core::Formatters::BaseTextFormatter
+  RSpec::Core::Formatters.register self, :dump_summary
+
+  def dump_summary(summary)
+    failed_files = summary.failed_examples.map { |e| RSpec::Core::Metadata::relative_path(e.file_path) }.uniq
+
+    return if summary.failed_examples.empty?
+
+    if summary.failed_examples.length <= 10
+      output.puts
+      output.puts 'Rerun all failed examples:'
+      output.puts
+      output.puts failure_colored("rspec #{failed_example_output(summary.failed_examples)}")
+    elsif failed_files.count <= 10
+      output.puts
+      output.puts 'Rerun all files containing failures:'
+      output.puts
+
+      output.puts failure_colored("rspec #{failed_files.join(' ')}")
+    end
+  end
+
+  private
+
+  def failed_example_output(failed_examples)
+    line_number_hash = Hash.new { |h, k| h[k] = [] }
+    failed_examples.each do |example|
+      line_number_hash[example.metadata.fetch(:file_path)] << example.metadata.fetch(:line_number)
+    end
+
+    line_number_hash.map do |file_path, line_numbers|
+      "#{file_path}:{#{line_numbers.join(',')}}"
+    end.join(' ')
+  end
+
+  def failure_colored(str)
+    RSpec::Core::Formatters::ConsoleCodes.wrap(str, :failure)
+  end
+end
